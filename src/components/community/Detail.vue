1894b423cdd3636806cff36a325781664611dfa3
junkukaka <342421055@qq.com>
2021-03-10 17:02:51 +0800

    asd

--- src/components/community/Detail.vue
+++ src/components/community/Detail.vue
@@ -1,56 +1,53 @@
 <template>
   <v-card flat>
-    <v-card flat >
-        <v-list-item three-line>
-            <v-list-item-content>
-                <div class="overline mb-2" style="color: rgba(0, 0, 0, 0.6)">
-                {{community.registerTime | date-format('yyyy-mm-dd hh:mi:ss')}}
-                </div>
-                <v-list-item-title class="headline mb-3 text-h4">
-                {{ community.title }}
-                </v-list-item-title>
-                <v-list-item-subtitle>
-                <span class="mr-3"> 100 liks</span>
-                <span class="mr-3"> 648 Views</span>
-                <span class="mr-3"> {{commentCount}} Comments</span>
-                </v-list-item-subtitle>
-            </v-list-item-content>
-        </v-list-item>
+    <v-card flat>
+      <v-list-item three-line>
+        <v-list-item-content>
+          <div class="overline mb-2" style="color: rgba(0, 0, 0, 0.6)">
+            {{community.registerTime | date-format('yyyy-mm-dd hh:mi:ss')}}
+          </div>
+          <v-list-item-title class="headline mb-3 text-h4">
+            {{ community.title }}
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            <span class="mr-3"> {{comInfoCount.hitsCount}} Views</span>
+            <span class="mr-3"> {{comInfoCount.collectCount}} Comments</span>
+            <span class="mr-3"> {{comInfoCount.likesCount}} liks</span>
+            <span class="mr-3"> {{comInfoCount.collectCount}} collect</span>
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
 
-        <v-card-actions style="padding: 0px">
+      <v-card-actions style="padding: 0px">
         <v-list-item class="grow">
-            <v-list-item-avatar color="grey darken-3">
+          <v-list-item-avatar color="grey darken-3">
             <v-img
-                class="elevation-6"
-                alt=""
-                src="https://cdn.vuetifyjs.com/images/lists/1.jpg"
+              class="elevation-6"
+              alt=""
+              src="https://cdn.vuetifyjs.com/images/lists/1.jpg"
             ></v-img>
-            </v-list-item-avatar>
+          </v-list-item-avatar>
 
-            <v-list-item-content>
+          <v-list-item-content>
             <v-list-item-title> 남광준 </v-list-item-title>
-            </v-list-item-content>
+          </v-list-item-content>
 
-            <v-row align="center" justify="end">
-              <router-link :to="`/communityEdit?id=${community.id}`">
-                  <v-icon class="mr-2">mdi-pencil</v-icon>
-                  <span class="subheading mr-2">수정</span>
-              </router-link>
-              <span>
-                    <v-icon class="mr-2"> mdi-domain </v-icon>
-                    <span class="subheading mr-2">: 운영팀</span>
-              </span>
-            </v-row>
+          <v-row align="center" justify="end">
+            <span>
+              <v-icon class="mr-2"> mdi-domain </v-icon>
+              <span class="subheading mr-2">: 운영팀</span>
+            </span>
+          </v-row>
         </v-list-item>
-        </v-card-actions>
+      </v-card-actions>
 
-        <v-divider class="mb-7 ml-3"></v-divider>
-        <div
-            id="read"
-            class="text-justify mb-3 ml-3"
-            v-html="community.content"
-            ></div>
-        <v-divider></v-divider>
+      <v-divider class="mb-7 ml-3"></v-divider>
+      <div
+        id="read"
+        class="text-justify mb-3 ml-3"
+        v-html="community.content"
+      ></div>
+      <v-divider></v-divider>
     </v-card>
 
     <!-- comment write area  -->
@@ -63,14 +60,19 @@
             value="The Woodman set to work at once, and so sharp was his axe that the tree was soon chopped nearly through."
             v-model="comment.content"
             :rules="commentRules"
-            @blur="reset"
           ></v-textarea>
         </v-form>
-        <v-btn color="primary" depressed  @click="commentSave"
-          dark >
-          <v-icon>mdi-pencil</v-icon>
-          댓글
-        </v-btn>
+        <div class="clearfix">
+          <v-btn
+            color="primary"
+            depressed
+            @click="commentSave"
+            class="float-right"
+            dark
+          >
+            <v-icon>mdi-pencil</v-icon> 댓글</v-btn
+          >
+        </div>
       </v-card>
 
       <!-- pop -->
@@ -78,11 +80,10 @@
         <v-card>
           <v-card-title style="">댓글등록</v-card-title>
           <v-card-actions>
-            <v-btn color="primary" text @click="dialog = false">Close</v-btn>
+            <v-btn color="primary" text @click="closeCommentAlert">Close</v-btn>
           </v-card-actions>
         </v-card>
       </v-dialog>
-
     </v-card>
 
     <!-- comment list area -->
@@ -90,43 +91,63 @@
       <div class="transition-swing text-h5 mb-1 mt-10 ml-3">Comments list</div>
       <v-divider class="ml-3"></v-divider>
       <v-list>
-          <template v-for="(item, index) in commentList">
-            <v-divider
-              v-if="item.divider"
-              :key="index"
-              :inset="item.inset"
-            ></v-divider>
-            <v-list-item
-               v-else
-              :key="index"
-            >
-              <v-list-item-avatar>
-                <v-img :src="item.picture"></v-img>
-              </v-list-item-avatar>
+        <template v-for="(item, index) in commentList">
+          <v-divider
+            v-if="item.divider"
+            :key="index"
+            :inset="item.inset"
+          ></v-divider>
+          <v-list-item v-else :key="index">
+            <v-list-item-avatar>
+              <v-img :src="item.picture"></v-img>
+            </v-list-item-avatar>
 
-              <v-list-item-content>
-                <v-list-item-title>{{item.user_name}}</v-list-item-title>
-                <v-list-item-subtitle>{{item.content}}</v-list-item-subtitle>
-              </v-list-item-content>
+            <v-list-item-content>
+              <v-list-item-title>{{ item.user_name }}</v-list-item-title>
+              <v-list-item-subtitle>{{ item.content }}</v-list-item-subtitle>
+            </v-list-item-content>
 
-              <v-list-item-action>
-                <v-list-item-subtitle>
-                  {{item.register_time|date-format('yyyy-mm-dd')}}
-                </v-list-item-subtitle>
-                <v-list-item-subtitle>
-               
-                </v-list-item-subtitle>
-              </v-list-item-action>
-              
-            </v-list-item>
-          </template>
-        </v-list>
+            <v-list-item-action>
+              <v-list-item-subtitle>
+                {{ item.register_time | date-format("yyyy-mm-dd") }}
+              </v-list-item-subtitle>
+              <v-list-item-subtitle> </v-list-item-subtitle>
+            </v-list-item-action>
+          </v-list-item>
+        </template>
+      </v-list>
     </v-card>
+
+    <!-- icon dial -->
+    <v-speed-dial
+      v-model="fab"
+      right
+      direction="top"
+      open-on-hover
+      transition="slide-y-reverse-transition"
+      :style="{ position: 'absolute', top: `${scrollTop + 200}px` }"
+    >
+      <template v-slot:activator>
+        <v-btn v-model="fab" color="blue darken-2" dark fab>
+          <v-icon v-if="fab"> mdi-close </v-icon>
+          <v-icon v-else> mdi-account-circle </v-icon>
+        </v-btn>
+      </template>
+      <v-btn fab dark small color="green" @click="saveCollect">
+        <v-icon>mdi-star</v-icon>
+      </v-btn>
+      <v-btn fab dark small color="red" @click="saveLikes">
+        <v-icon>mdi-thumb-up</v-icon>
+      </v-btn>
+    </v-speed-dial>
+  </v-card>
+</template>
+
   </v-card>
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
+import { mapGetters } from "vuex";
 
 export default {
   data: () => ({
@@ -138,33 +159,63 @@ export default {
     },
     dialog: false,
     valid: true,
+    fab: false,
+    scrollTop: 0,
     comment: {
       userId: null,
       communityId: null,
-      content: "",
-      reply_user_id: null
+      content: ""
     },
     commentRules: [
       (v) => !!v || "comment is required",
-      (v) => (v && v.length <= 200) || "comment must be less than 200 characters",
+      (v) =>
+        (v && v.length <= 200) || "comment must be less than 200 characters",
     ],
     commentList: [],
     commentCount: null,
+    comInfo:{
+      communityId: null,
+      userId: null
+    },
+    comInfoCount:{
+      hitsCount: 0,
+      likesCount: 0,
+      collectCount: 0,
+      commentCount: 0
+    }
   }),
 
   created() {
     this.community.id = this.$route.query.id;
+    //comment
     this.comment.communityId = this.$route.query.id;
     this.comment.userId = this.getUser().id; //用户ID 赋值
-    this.getCommunity();
+    //comInfo
+    this.comInfo.communityId = this.$route.query.id;
+    this.comInfo.userId = this.getUser().id; //用户ID 赋值
+    this.scrollTop = document.documentElement.scrollTop;
+    this.getCommunity(); 
     this.getComments();
+    this.saveHits(); 
+    this.getInfoCount(); 
+  },
+
+  
+
+  mounted() {
+    window.addEventListener("scroll", this.scrollHandle); //绑定页面滚动事件
   },
 
   methods: {
     ...mapGetters(["getUser"]),
 
+    scrollHandle(e) {
+      this.scrollTop = e.srcElement.scrollingElement.scrollTop; // 获取页面滚动高度
+      //console.log(this.scrollTop);
+    },
+
     getCommunity() {
-      //community select 
+      //community select
       this.$nextTick(function () {
         this.$http
           .get(`/community/communitys/${this.community.id}`)
@@ -174,7 +225,7 @@ export default {
       });
     },
 
-    //댓글 조회
+    //comment select
     getComments() {
       this.$nextTick(function () {
         this.$http
@@ -183,34 +234,67 @@ export default {
             const comments = response.data.data.list;
             this.commentCount = response.data.data.count;
             //下划线处理
-            for(let i = 0; i < comments.length ; i++){
-              this.commentList.push(comments[i])
-              this.commentList.push({divider: true, inset: true })
+            for (let i = 0; i < comments.length; i++) {
+              this.commentList.push(comments[i]);
+              this.commentList.push({ divider: true, inset: true });
             }
           });
       });
     },
 
+    //community info commont,likes,collect,hits count select 
+    getInfoCount(){
+      this.$nextTick(function(){
+        this.$http.get(`/comInfo/count/${this.community.id}`)
+        .then((response) => {
+          this.comInfoCount = response.data.data
+        })
+      })
+    },
+
     //comment save
-    commentSave(){
+    commentSave() {
       const val = this.$refs.form.validate(); // 验证
-      if(val){
-        this.$http
-          .post("/comment/comments",this.comment)
-          .then(response => {
-            //请求成功
-            if(response.data.data === 1){
-              //comment reset
-              this.comment.content = "";
-              this.dialog = true;
-            }
-          });
+      if (val) {
+        this.$http.post("/comment/comments", this.comment).then((response) => {
+          //请求成功
+          if (response.data.data === 1) {
+            //comment reset
+            this.comment.content = "";
+            this.dialog = true;
+          }
+        });
       }
     },
 
-    //comment 
-    reset () {
-        this.$refs.form.reset()
+    saveHits(){
+      this.$nextTick(function(){
+        this.$http.post("/comInfo/save/hits",this.comInfo)
+      })
+    },
+
+    saveLikes(){
+      this.$nextTick(function(){
+        this.$http.post("/comInfo/save/likes",this.comInfo)
+      })
+    },
+
+    saveCollect(){
+      this.$nextTick(function(){
+        this.$http.post("/comInfo/save/collect",this.comInfo)
+      })
+    },
+
+    //close comment alert 
+    closeCommentAlert(){
+      this.commentList = []
+      this.getComments();
+      this.reset();
+      this.dialog = false;
+    },
+    //comment
+    reset() {
+      this.$refs.form.reset();
     },
   },
 };
@@ -221,7 +305,10 @@ export default {
   background-color: rgb(236, 236, 236);
   padding: 10px;
 }
-.v-application a{
-    text-decoration: none;
+#read p img {
+  max-width: 100%;
+}
+.v-application a {
+  text-decoration: none;
 }
 </style>
